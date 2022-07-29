4adb9342609ce2ba4d41e1c71798609b6a349f29
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2022-07-29 10:52:54 -0400

    banner and exhibit added

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,7 +1,20 @@
+import { render } from "@testing-library/react";
 import "./App.css";
+import { Component } from "react";
+import Banner from "./Banner";
+import Exhibit from "./Exhibit";
 
-function App() {
-
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Banner bannerText="Sextant" />
+        <Exhibit name="Exhibit 1"></Exhibit>
+        <Exhibit name="Exhibit 2"></Exhibit>
+        <Exhibit name="Exibit 3"></Exhibit>
+      </div>
+    );
+  }
 }
 
 export default App;
